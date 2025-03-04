69ecec2a8b7e91d2fea7bec52543d1127e1472c7
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 13:04:05 +0800

    File name changed

---
+++ 1-variables-and-types.py
@@ -0,0 +1,26 @@
+# Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working
+# as an IT Technician / IT Support / System Administrator. I'm doing this for practice.
+
+# variables can't start with numbers
+# never start a variable name with an upper case because it can get confused with a class
+# you can use both single quotes and double quotes for strings in python
+
+variable1 = 69
+print(variable1)
+
+# you can use type() to identify the date type of something
+type(variable1)
+
+# you can concatenate strings using the + sign
+var1 = "Good morning, "
+var2 = "Mister "
+variable_concatenate_example = var1 + var2
+print(variable_concatenate_example)
+
+# adding "1" + 1 will result in error because they are different data types
+
+# Booleans start with a capital letter: True or False   - super important
+# because if you used a small letter, it will try to interpret it as a variable
+
+
+
