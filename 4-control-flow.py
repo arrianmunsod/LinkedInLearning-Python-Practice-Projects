a10e99aec2e0adac2b4bc45c98b74e71db3fb742
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 15:37:44 +0800

    Random Push

--- 4-control-flow.py
+++ 4-control-flow.py
@@ -1,7 +1,11 @@
 # Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working
 # as an IT Technician / IT Support / System Administrator. I'm doing this for practice.
 
+# If / Else statements
 
+a = False
+if a:
+    print("This is true!")
 
 
 
