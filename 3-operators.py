3d0e4fb2b35bcdcf4524d00e5c2c132ca9ac777c
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 14:39:05 +0800

    Initial push

--- 3-operators.py
+++ 3-operators.py
@@ -1,7 +1,18 @@
 # Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working
 # as an IT Technician / IT Support / System Administrator. I'm doing this for practice.
 
+# ARITHETIC OPERATORS
+print(70 + 30)
 
+# to type exponent, use **
+print(5 ** 2)
+
+# the modulus operator gives you the remainder after any division
+# use the sign   %    for modulus
+
+######################################################################################################
+
+# COMPARISON, LOGICAL, AND MEMBERSHIP OPERATORS
 
 
 
