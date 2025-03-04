19cde7eab52262234cf7a99936cf62d43bc5a75e
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 16:53:33 +0800

    Random Push

--- 6-classes-and-objects.py
+++ 6-classes-and-objects.py
@@ -1,3 +1,24 @@
 # Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working
 # as an IT Technician / IT Support / System Administrator. I'm doing this for practice.
 
+# we use classes and objects for re-usability
+# init function is called whenever an instance of the class Dog is created
+# self is a variable
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        self.legs = 4
+
+    def speak(self):
+        print(self.name + " says: Bark!")
+
+# giving the dog a name
+my_dog = Dog("Askal")
+another_dog = Dog("Browny")
+
+# another example of using the class
+# run the code to see the result
+my_dog.speak()
+
+# another instance
+another_dog.speak()
