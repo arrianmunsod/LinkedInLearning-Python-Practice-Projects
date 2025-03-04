6fc61b3331bb4b810a5a3fc74cfc4413184541cf
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 12:44:57 +0800

    01

--- variables-and-types.py
+++ variables-and-types.py
@@ -1,5 +1,13 @@
-x = 69
-print(x)
+# Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working working
+# as a System Administrator.
 
 # variables can't start with numbers
-#
+# never start a variable name with an upper case because it can get confused with a class
+# you can use both single quotes and double quotes for strings in python
+
+variable1 = 69
+print(variable1)
+
+# you can use type() to identify the date type of something
+type(variable1)
+
