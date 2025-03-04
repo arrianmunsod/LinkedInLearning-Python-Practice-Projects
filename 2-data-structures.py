b0e463afd35c44f021dc75775eba7f3d3b92d473
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 13:19:57 +0800

    Initial push

--- 2-data-structures.py
+++ 2-data-structures.py
@@ -1,4 +1,32 @@
 # Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working
 # as an IT Technician / IT Support / System Administrator. I'm doing this for practice.
 
-#
+# List is represented by opening and closing bracket
+my_list = [1,2,3,4]
+print(my_list)
+
+# you can have integers, strings, floats, booleans, or another list inside a list
+# you can have nested lists
+my_list_02 = ["banana", False, 24]
+print(len(my_list_02))
+# len() count how many values are inside the list
+
+
+my_list_03 = [[1,2,3],[False,True],["basketball","football"]]
+
+###################################################################################################
+
+# Set is almost identical to list, except that all of the elements in it have to be unique
+# curly bracket is used
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+
+
+
+
+
+
+
+
+
