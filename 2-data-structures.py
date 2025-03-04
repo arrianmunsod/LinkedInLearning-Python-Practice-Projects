7a47318141560f0ab13ecf5c4a6b896447cec80c
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 13:10:15 +0800

    Initial push

--- 2-data-structures.py
+++ 2-data-structures.py
@@ -1,3 +1,4 @@
 # Yeah, I know. What I'm doing is super basic. I haven't coded in years, since I've been working
 # as an IT Technician / IT Support / System Administrator. I'm doing this for practice.
 
+#
