947894730b5b65a623fdc5a4b764d46f29405cfa
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-04 13:46:26 +0800

    Initial push

--- 2-data-structures.py
+++ 2-data-structures.py
@@ -14,6 +14,8 @@ print(len(my_list_02))
 
 my_list_03 = [[1,2,3],[False,True],["basketball","football"]]
 
+# the order of the elements inside a list is important if it is compared to another list
+
 ###################################################################################################
 
 # Set is almost identical to list, except that all of the elements in it have to be unique
@@ -21,9 +23,27 @@ my_list_03 = [[1,2,3],[False,True],["basketball","football"]]
 my_set = {1, 2, 3, 4, 5}
 print(my_set)
 
+###################################################################################################
 
+# Tuples is similar to list, but they are enclosed by parenthesis
+# order also matters in tuple when comparing to another tuple
+# you cannot modify tuples
+# once a tuple is declared, you can't add to it or change any of the values in it
+# Tuples are used because they are memory efficient
+# It uses only exactly the amount of memory it needs, good for storing little things like coordinates
+my_tuple = (1,2,3)
 
+###################################################################################################
 
+# Dictionaries in python is similar to dictionaries in real world
+# the keys in dictionaries need to be unique
+# uses curly brackets
+my_dictionary = {
+    "apple": "A red fruit",
+    "banana": "Contains lots of potassium"
+}
+
+print(my_dictionary["apple"])
 
 
 
